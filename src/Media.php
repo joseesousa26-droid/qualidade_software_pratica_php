27710ced25aec2224f82ddd19babe4621a7ee6dc
Okarun <jose.e.sousa26@aluno.senai.br>
2026-04-16 15:24:36 -0300

    add: GitHub Actions tests

--- src/Media.php
+++ src/Media.php
@@ -1,11 +1,11 @@
 <?php
 class Media {
-public function calcularMedia($notas) {
-// ERRO proposital: soma mal implementada
-$soma = 1;
-foreach ($notas as $nota) {
-$soma += $nota;
-}
-return $soma / count($notas);
-}
+    public function calcularMedia($notas) {
+        $soma = 0;
+        foreach ($notas as $nota) {
+            $soma += $nota;
+        }
+        // O round() remove as dízimas que estão quebrando o teste
+        return ($soma / count($notas)); 
+    }
 }
